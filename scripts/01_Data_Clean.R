12c9b21bece114ef31e4a8b50aa08bbafe2726cb
Frankie Gerraty <frankiegerraty@gmail.com>
2024-02-21 17:07:41 -0800

    working

--- scripts/01_Data_Clean.R
+++ scripts/01_Data_Clean.R
@@ -5,9 +5,9 @@
 # Script 01: Data Clean ##################################################
 #-------------------------------------------------------------------------
 
-####################################
-# PART 1: Import and Clean Data ####
-####################################
+###############################################
+# PART 1: Import and Clean Seal Count Data ####
+###############################################
 
 # Import and clean PORE harbor seal population data (3 sites) ---------
 
@@ -16,13 +16,14 @@ PRNS_seals <- read_excel("data/raw/PRNS_HarborSealCounts_2010_2022.xlsx") %>%
   mutate(time = hms::as_hms(time), #fix time format
          #replace values in age column with lower case
          age = replace(age, age == "ADULT", "adult"),
-         age = replace(age, age == "PUP", "pup")) %>%  
+         age = replace(age, age == "PUP", "pup"),
+         year = year(date)) %>%  
   #Filter for only living individuals from the breeding/pupping season
   filter(age == "adult" |
          age == "pup")
 
 
-# Import and clean MacKerricher harbor seal population data (1 site) ---------
+# Import and clean Pre-2018 MacKerricher harbor seal population data (1 site) ---------
 MC_seals_pre2018 <- read_excel("data/raw/Mackerricher_HASE_Counts_Pre2018.xlsx") %>% 
   clean_names() %>% 
   filter(location == "R") %>% 
@@ -31,10 +32,49 @@ MC_seals_pre2018 <- read_excel("data/raw/Mackerricher_HASE_Counts_Pre2018.xlsx")
                names_to = 'age', 
                values_to = "count") %>% 
   mutate(time = hms::as_hms(time), #fix time format
-         subsite = "MC")
+         subsite = "MC",
+         year = year(date))
+
+# Import and clean MacKerricher harbor seal population data (1 site) ---------
+MC_seals_post2018 <- read_excel("data/raw/MacKerricher_seal_summary_post2018.xlsx") %>% 
+  select(year, annual_adult_max, annual_pup_max) %>% 
+  pivot_longer(cols = c(annual_adult_max, annual_pup_max), 
+               names_to = "age", values_to = "count") %>% 
+  mutate(age = if_else(age =="annual_adult_max", "adult", "pup"),
+         subsite = "MC", date = NA, time = NA) 
 
 #Combine all seal data together and export ------------------------------------
-seals <- rbind(PRNS_seals, MC_seals_pre2018)
+seals <- rbind(PRNS_seals, MC_seals_pre2018, MC_seals_post2018)
 
 write_csv(seals, "data/clean/seals.csv")
   
+
+
+###################################################
+# PART 1: Import and Clean Seal Predation Data ####
+###################################################
+
+MC_seal_mortality <- read_excel("data/raw/MacKerricher_mortality.xlsx") %>% 
+  clean_names() %>% 
+  mutate(date = ymd(observation_date),
+         day = yday(date),
+         year = year(date),
+         subsite = "MC") %>% 
+  select(subsite, day, year) %>% 
+  group_by(subsite, day, year) %>% 
+  summarize(count = n(), .groups = "drop")
+
+PRNS_seal_mortality <- read_excel("data/raw/PRNS_mortality.xlsx") %>% 
+  clean_names() %>% 
+  filter(type_of_event %in% c("Observed predation", "Suspected predation attempt and Scavenging")) %>% 
+  mutate(subsite = site, 
+         day = yday(start_date),
+         year = year(start_date)) %>% 
+  select(subsite, day, year) %>% 
+  group_by(subsite, day, year) %>% 
+  summarize(count = n(), .groups = "drop")
+
+#Combine all seal data together and export ------------------------------------
+mortality <- rbind(MC_seal_mortality, PRNS_seal_mortality)
+
+write_csv(mortality, "data/clean/mortality.csv")
--- scripts/0X_exploration.R
+++ scripts/0X_exploration.R
@@ -8,12 +8,12 @@
 seals <- read_csv("data/clean/seals.csv") %>% 
   mutate(subsite = factor(subsite, levels = c("MC", "DE", "DP", "BL")))
 
+site_names <- c(MC = "MacKerricher", DE = "Drake's Estero", DP = "Double Point", BL = "Bolinas Lagoon")
 
 #Annual trends of seal counts ############
 
 annual_summary <- seals %>% 
   #Filter for only living individuals from the breeding/pupping season
-  mutate(year = year(date)) %>% 
   group_by(year, age, subsite) %>% 
   summarise(max_number = max(count, na.rm = TRUE), .groups = "drop")
 
@@ -35,23 +35,53 @@ ggsave("output/annual_summary.png", width = 7.5, height = 10, units = "in")
 
 #Trends of Seal Pup Timing ##################
 
-site_names <- c(MC = "MacKerricher", DE = "Drake's Estero", DP = "Double Point", BL = "Bolinas Lagoon")
-
 timing_summary <- seals %>% 
   filter(age == "pup") %>% 
   mutate(day = yday(date),
          year = factor(year(date)),
-         count = ifelse(is.na(count), 0, count)) 
+         count = ifelse(is.na(count), 0, count)) %>% 
+  filter(day>80 & day <165)
 
-ggplot(timing_summary, aes(x=day, y=count, color = year))+
-  geom_point()+
+ggplot(timing_summary, aes(x=day, y=count))+
+  geom_point(alpha = .7, shape=16)+
+  geom_vline(data = MC_seal_mortality, 
+             aes(xintercept = day, size=count, color = "red")) +
+  geom_vline(data = PRNS_seal_mortality, 
+             aes(xintercept = day, size=count, color = "red")) +
   facet_wrap(facets = "subsite", 
              ncol = 1, scales = "free_y", 
              labeller = labeller(subsite = site_names))+
-  scale_color_viridis_d()+
   theme_few()+
-  labs(y = "# Pups Observed", x="Day of Year", fill = "Year")
+  labs(y = "# Pups Observed", x="Day of Year")+
+  geom_smooth(method = "gam", color = "black")+
+  scale_size_continuous(range = c(.5,1.5))+
+  theme(legend.position = "none")
 
 ggsave("output/timing_summary.png", width = 7.5, height = 10, units = "in")
 
   
+
+MC_timing <- timing_summary %>% 
+  filter(subsite == "MC")
+
+ggplot(MC_timing, aes(x=day, y=count))+
+  geom_point(alpha = .7, shape=16)+
+  geom_vline(data = MC_seal_mortality, 
+             aes(xintercept = day, 
+                 size=count 
+                 
+                 ),
+             color = "red") +
+  theme_few()+
+  labs(y = "# Pups Observed (all years)", x="Day of Year", size = "# Pups Killed\nper Julian Date\n(sum of all years)")+
+  geom_smooth(method = "gam", color = "black")+
+  scale_size_continuous(range = c(.5,1.5), 
+                        breaks= c(1,2,3,4),
+                        labels = c(1,2,3,4))+
+  guides(size=guide_legend(override.aes=list(colour="red")))+
+  coord_cartesian(ylim = c(-2,50))
+
+
++
+#  theme(legend.position = "none")+
+#  scale_color_viridis()
